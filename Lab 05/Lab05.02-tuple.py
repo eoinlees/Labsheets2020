3779c7278b4c2db096c3190fd8812b756b1d82d9
Eoin <eoinlees@outlook.com>
2020-03-25 22:19:51 +0000

    Lab 05 added

---
+++ Lab 05/Lab05.02-tuple.py
@@ -0,0 +1,19 @@
+# Eoin Lees
+# this program creates a tuple that prints the summer months
+
+months = ("January", 
+    "February", 
+    "March", 
+    "April", 
+    "May", 
+    "June", 
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December")
+
+summer = months[4:7]
+for month in summer:
+    print(month)
